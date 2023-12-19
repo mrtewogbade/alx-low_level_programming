3dc4bd129e1a5e940da5b19159596236ac3dba0b
mrtewogbade <johndharey@gmail.com>
2023-12-19 23:12:42 +0100

    read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,52 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
+#include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * read_textfile - program reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
  *
- * @filename: The name of the file to read.
- * @letters: The number of letters to read and print.
- *
- * Return: The actual number of letters it could read and print, or 0 on failure.
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t nread, nwritten;
-	char buf[BUFSIZ];
+	ssize_t nrd, nwr;
+	char *buf;
 
-	if (filename == NULL)
-		return 0;
+	if (!filename)
+		return (0);
 
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
-		return 0;
+		return (0);
 
-	while ((nread = read(fd, buf, BUFSIZ)) > 0)
-	{
-		if (letters > 0 && nread > letters)
-			nread = letters;
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
-		nwritten = write(STDOUT_FILENO, buf, nread);
-		if (nwritten != nread)
-		{
-			close(fd);
-			return 0;
-		}
-
-		letters -= nread;
-		if (letters == 0)
-			break;
-	}
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
 	close(fd);
 
-	if (nread == -1)
-		return 0;
+	free(buf);
 
-	return nread;
+	return (nwr);
 }
-
